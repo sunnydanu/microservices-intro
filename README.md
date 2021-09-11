ac147999eb9f08ff71aa6d30aad140d6faa8d067
sunny danu <mail@sunnydanu.com>
2021-09-11 10:12:42 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# mircorservice-intro-blog
---
+++ moderation/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const axios = require('axios')
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.post('/events', async (req, res) => {
+    console.log('Recevied Event', req.body.type);
+
+    const { type, data } = req.body;
+    if (type === 'CommentCreated') {
+
+        const status = data.content.includes('orange') ? "reject" : "approved";
+
+
+        await axios.post('http://localhost:4005/events', {
+            type: 'CommentModerated',
+            data: {
+                id: data.id,
+                postId: data.postId,
+                status,
+                content: data.content,
+            },
+        })
+
+    }
+    res.send({})
+ 
+})
+
+app.listen(4003, console.log('listening on http://localhost:4003'))
