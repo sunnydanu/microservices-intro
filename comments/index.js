39d2017305b244c15d10226cddee5cb858ab1cdb
sunny danu <mail@sunnydanu.com>
2021-09-11 15:33:58 +0530

    ClusterIP Services of  Query, Moderation and Comments

--- comments/index.js
+++ comments/index.js
@@ -28,7 +28,7 @@ app.post('/posts/:id/comments', async function (req, res) {
     commentsByPostId[req.params.id] = comments;
 
 
-    await axios.post('http://localhost:4005/events', {
+    await axios.post('http://event-bus-srv:4005/events', {
         type: 'CommentCreated',
         data: newComment
     })
@@ -50,7 +50,7 @@ app.post('/events', async (req, res) => {
         const comment = comments.find(comment => comment.id === id);
         comment.status = status;
 
-        await axios.post('http://localhost:4005/events', {
+        await axios.post('http://event-bus-srv:4005/events', {
             type: 'CommentUpdated',
             data: { id, status, postId, content },
         })
